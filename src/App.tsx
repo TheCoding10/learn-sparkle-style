1d1722a6840bb6b6e79b4919da6244b7e1d4933a
TheCoding10 <prayasthapa0522@gmail.com>
2025-09-19 18:43:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,38 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+// Import your course pages
+import Math from "./pages/Math";
+import CS from "./pages/CS";
+import Finance from "./pages/Finance";
+import DataScience from "./pages/DataScience";
+
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            {/* Homepage */}
+            <Route path="/" element={<Index />} />
+
+            {/* Course Pages */}
+            <Route path="/math" element={<Math />} />
+            <Route path="/computer-science" element={<CS />} />
+            <Route path="/finance" element={<Finance />} />
+            <Route path="/data-science" element={<DataScience />} />
+
+            {/* Catch-all for 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
