4ea62006445b854c49a5790a559a611e4d1ddd2c
TheCoding10 <prayasthapa0522@gmail.com>
2025-09-19 18:58:10 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -20,21 +20,19 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            {/* Homepage */}
-            <Route path="/" element={<Index />} />
+        <Routes>
+          {/* Homepage */}
+          <Route path="/" element={<Index />} />
 
-            {/* Course Pages */}
-            <Route path="/math" element={<Math />} />
-            <Route path="/computer-science" element={<CS />} />
-            <Route path="/finance" element={<Finance />} />
-            <Route path="/data-science" element={<DataScience />} />
+          {/* Course Pages */}
+          <Route path="/math" element={<Math />} />
+          <Route path="/computer-science" element={<CS />} />
+          <Route path="/finance" element={<Finance />} />
+          <Route path="/data-science" element={<DataScience />} />
 
-            {/* Catch-all for 404 */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+          {/* Catch-all for 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </TooltipProvider>
     </QueryClientProvider>
   );
