add7abf7157fadf59427a5fe7b818f1ab6184766
TheCoding10 <prayasthapa0522@gmail.com>
2025-09-28 20:23:00 -0400

    Update and rename DataScience.tsx to Physics.tsx

---
+++ src/pages/Physics.tsx
@@ -0,0 +1,8 @@
+export default function Physics() {
+  return (
+    <div>
+      <h1>Physics</h1>
+      <p>Master classical mechanics, E&M, quantum theory, and relativity.</p>
+    </div>
+  );
+}
